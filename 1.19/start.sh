dca46fce21c4d3a7a07eed5dcc9fe5c1b1b7e1b2
AsP3X <zombymedia@gmail.com>
2022-08-26 20:53:30 +0200

    TASK: added 1.19 to available the versions

---
+++ 1.19/start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+####################################################################
+## ZOUPA - (ZombyMediaIC open source usage protection agreement)  ##
+## License as of: 15.03.2022 11:10 | #202203151110                ##
+## Niklas Vorberg (AsP3X)                                         ##
+####################################################################
+
+# echo "###############################################"
+# echo "###############################################"
+# echo "       Warning this build is deprecated        "
+# echo "  and will no longer be updated and supported  "
+# echo "                                               "
+# echo "     Do not under ANY CIRCUMSTANCES use it     "
+# echo "     for production or anything else exept     "
+# echo "       testing and private purposes only       "
+# echo "###############################################"
+# echo "###############################################"
+
+sleep 4
+
+# split the number and the character G
+
+if [[ "${RAM}" ]]; then
+  rm -rf /server/user_jvm_args.txt
+  touch /server/user_jvm_args.txt
+	echo "Using ${RAM}"
+  echo "-Xmx${RAM} -Xms${RAM}" >> /server/user_jvm_args.txt
+else
+	echo "Set ram to default of 4G"
+	echo "This can be changed later"
+	RAM=4G
+  touch /server/user_jvm_args.txt
+	echo "Using ${RAM}"
+  echo "-Xmx${RAM} -Xms${RAM}" >> /server/user_jvm_args.txt
+fi
+sleep 1
+
+SERVERFILE=/server/run.sh
+if test -f "$SERVERFILE"; then
+  echo "Starting server..."
+	# screen -S Minecraft-Server /bin/sh -c "java -Xmx${RAM} -Xms${RAM} -jar papermc.jar"
+  screen -S Minecraft-Server /bin/sh -c "/server/run.sh"
+else
+  echo "Creating new Files"
+	sleep 1
+	# screen -S Minecraft-Server /bin/sh -c "java -Xmx${RAM} -Xms${RAM} -jar papermc.jar"
+  screen -S Minecraft-Server /bin/sh -c "/server/run.sh"
+fi
